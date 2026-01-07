a92d449556f415130b31cd249380a63347fce844
semyonov12 <68299513+semyonov12@users.noreply.github.com>
2026-01-07 10:13:26 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
-(() => {
-    "use strict";
-    let documentBody = document.documentElement;
+document.addEventListener("DOMContentLoaded", function (event) {
+
+	let documentBody = document.documentElement;
     const play = document.querySelector(".hero__button");
-    play?.addEventListener("click", function (e) {
-        const audio = document.querySelector('.audio');
-        if (audio) {
-            if (audio.paused) {
-                documentBody.classList.add('play');
-                audio.play();
-            } else {
-                documentBody.classList.remove('play');
-                audio.pause();
-            }
-        }
-    });
-
-
     
-})();
+    play?.addEventListener("click", (function(e) {
+        const audio = document.querySelector(".audio");
+        if (audio) if (audio.paused) {
+            documentBody.classList.add("play");
+            audio.play();
+        } else {
+            documentBody.classList.remove("play");
+            audio.pause();
+        }
+    }));
+
+});
